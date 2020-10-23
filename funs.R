708ce5832918b8e4e1715e2f941d503cffba7fb9
mthomas-ketchbrook <52801195+mthomas-ketchbrook@users.noreply.github.com>
2020-10-23 10:47:51 -0400

    Added info cards & chart download functionality

--- funs.R
+++ funs.R
@@ -32,7 +32,8 @@ generate_calendar_viz <- function(data, var) {
           return('date: ' + params.value[0] + '<br />value: ' + params.value[1])
         }
       ")
-    ) %>%
+    ) %>% 
+    e_toolbox_feature(feature = "saveAsImage") %>% 
     echarts4r::e_brush(throttleDelay = 1000)
   
 }
@@ -50,7 +51,8 @@ generate_bar_chart <- function(data, var) {
     tidyr::drop_na() %>% 
     echarts4r::e_chart(x = date) %>% 
     echarts4r::e_bar_(serie = var_name, name = var) %>% 
-    echarts4r::e_tooltip(trigger = "item")
+    echarts4r::e_tooltip(trigger = "item") %>% 
+    e_toolbox_feature(feature = "saveAsImage")
   
 }
 
@@ -66,6 +68,25 @@ generate_area_chart <- function(data, var) {
     tidyr::drop_na() %>% 
     echarts4r::e_chart(x = date) %>% 
     echarts4r::e_area_(serie = var_name, name = var) %>% 
-    echarts4r::e_tooltip(trigger = "axis")
+    echarts4r::e_tooltip(trigger = "axis") %>% 
+    e_toolbox_feature(feature = "saveAsImage")
+  
+}
+
+
+create_info_card <- function(header, main, subtext, fill = NULL) {
+  
+  if (!is.null(fill)) {
+    
+    fill <- paste0("background: ", fill)
+    
+  }
+  
+  shiny::wellPanel(
+    style = fill, 
+    shiny::h4(header), 
+    shiny::h2(main), 
+    shiny::p(subtext)
+  )
   
 }
